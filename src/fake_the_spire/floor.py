c39a0522366f00d4b0738190ba5fb5657b47fc57
Akiva-Notkin <akiva.notkin@gmail.com>
2024-05-12 15:47:17 -0700

    Make tests better and fix bugs found with better tests

--- src/fake_the_spire/floor.py
+++ src/fake_the_spire/floor.py
@@ -2,20 +2,19 @@
 class Floor:
     def __init__(self, game_state: dict):
         self.game_state = game_state
-        self.should_drop_potion = False
         self.can_remove_card = False
 
     def get_new_options(self) -> (list[str], int):
         options = []
-        if self.should_drop_potion:
+        if len(self.game_state['player']['potions']) > self.game_state['player']['max_potions']:
             for potion in self.game_state['player']['potions']:
                 options.append(f"drop {potion}")
-            return options
+            return options, 1
 
         if self.can_remove_card:
             for card in self.game_state['player']['deck'].keys():
                 options.append(f"remove {card}")
-            return options
+            return options, 1
 
     def to_dict(self):
         ...
@@ -26,10 +25,6 @@ class Floor:
     def drop_potion(self, action: list[str]):
         potion = action[0]
         self.game_state['player']['potions'].remove(potion)
-        if len(self.game_state['player']['potions']) > self.game_state['player']['max_potions']:
-            self.should_drop_potion = True
-        else:
-            self.should_drop_potion = False
 
     def remove_card(self, action: list[str]):
         card_to_remove = action[0]
@@ -39,6 +34,7 @@ class Floor:
                 if self.game_state['player']['deck'][card] == 0:
                     self.can_remove_card = False
                     del self.game_state['player']['deck'][card]
+                    break
 
 
 
